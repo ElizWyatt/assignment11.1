b6cb01ab507f8f1b26588e8a5274bca41e932d6a
ElizWyatt <53824368+ElizWyatt@users.noreply.github.com>
2019-08-19 19:00:14 -0400

    Add files via upload
    first commit

---
+++ assignment11.1/script.js
@@ -0,0 +1,17 @@
+
+var tag = document.querySelector('p');
+console.log(tag);
+
+var id = document.getElementById('first');
+console.log(id);
+
+var className = document.getElementsByClassName('special');
+console.log(className);
+
+var tagName = document.getElementsByTagName('p');
+console.log(tagName);
+
+
+
+
+
